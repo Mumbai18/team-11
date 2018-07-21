1c24cb9225976da46f0f54374e818ee09283932c
Shruti Maheshwari <2016shruti.maheshwari@ves.ac.in>
2018-07-22 02:19:04 +0530

    send sms

---
+++ php/sendsms.php
@@ -0,0 +1,12 @@
+<?php
+include('way2sms-api.php');
+
+if (isset($_REQUEST['v_contact']) && isset($_REQUEST['msg'])) {
+    $res = sendWay2SMS($_REQUEST['v_contact'], $_REQUEST['msg']);
+    if (is_array($res))
+        echo $res[0]['result'] ? 'true' : 'false';
+    else
+        echo $res;
+    exit;
+}
+?>
