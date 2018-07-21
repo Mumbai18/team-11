76d47b10c0c78fccf9e6aded02b17e8af650efc3
sadia <sas170298@gmail.com>
2018-07-22 01:39:37 +0530

    nearest volunteers updated

--- php/nearest.php
+++ php/nearest.php
@@ -10,6 +10,8 @@ else
 
 <?php
 //echo "hello";
+header("Content-Type: application/json; charset=UTF-8");
+$output=array();
 $id=1;
 $loc=mysqli_query($con,"select * from donor where d_id='$id'");
 $count=mysqli_num_rows($loc);
@@ -17,27 +19,34 @@ $count=mysqli_num_rows($loc);
 			{
 				while($row = mysqli_fetch_array($loc))
 				{
-					$d_loc=$row['d_address'];
-				
-		
+					$d_loc=$row['d_address'];		
 				}
 			}
 
 
 
-$sql=mysqli_query($con,"select * from volunteer where v_address='$d_loc'");
+$sql=mysqli_query($con,"select * from volunteer where v_address LIKE '%$d_loc%'");
 $count=mysqli_num_rows($sql);
 			if ($count > 0) 
-			{
+			{ $i = 0;
 				while($row = mysqli_fetch_array($sql))
 				{
 					$v_loc=$row['v_address'];
 					$v_contact=$row['v_contact'];
-					echo "$v_loc";
-					echo "\n";				
-					echo "$v_contact";
-					echo "<br>";
+					sendMsg($v_contact);
+					$v_id=$row['v_id'];
+					
+					$output[$i++]=$v_loc;
+					
 				}
+				echo json_encode($output);
+			}
+			
+			function sendMsg($v_contact)
+			{
+				
+				//echo "hello".$v_contact;
+				
 			}
 			
 			
