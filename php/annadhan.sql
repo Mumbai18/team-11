bae0b1caa34bf59c5f037752c4ce4e5990479c18
Shruti Maheshwari <2016shruti.maheshwari@ves.ac.in>
2018-07-22 02:49:36 +0530

    database update

--- php/annadhan.sql
+++ php/annadhan.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.7.0
+-- version 4.7.4
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 21, 2018 at 07:49 PM
--- Server version: 10.1.22-MariaDB
--- PHP Version: 7.1.4
+-- Generation Time: Jul 21, 2018 at 11:17 PM
+-- Server version: 10.1.28-MariaDB
+-- PHP Version: 7.0.24
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -33,7 +33,7 @@ CREATE TABLE `donor` (
   `d_firstname` varchar(60) NOT NULL,
   `d_secondname` varchar(50) NOT NULL,
   `d_email` varchar(20) NOT NULL,
-  `d_contact` bigint(20) NOT NULL,
+  `d_contact` int(20) NOT NULL,
   `d_address` varchar(60) NOT NULL,
   `d_password` varchar(30) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
@@ -43,7 +43,7 @@ CREATE TABLE `donor` (
 --
 
 INSERT INTO `donor` (`d_id`, `d_firstname`, `d_secondname`, `d_email`, `d_contact`, `d_address`, `d_password`) VALUES
-(1, 'rani', 'rr', 'rrrr', 555, 'borivali', 'uuuu');
+(1, 'shruti', 'maheshwari', 'shruti@gmail.com', 64764746, 'chembur', 's1234');
 
 -- --------------------------------------------------------
 
@@ -57,7 +57,7 @@ CREATE TABLE `food` (
   `packaging` varchar(100) NOT NULL,
   `location` varchar(200) NOT NULL,
   `picture` blob NOT NULL,
-  `pickup_time` int(11) NOT NULL,
+  `pickup_time` time(6) NOT NULL,
   `volunteer` varchar(100) NOT NULL,
   `donor` varchar(100) NOT NULL,
   `success` varchar(50) NOT NULL
@@ -85,20 +85,10 @@ CREATE TABLE `volunteer` (
   `v_firstname` varchar(60) NOT NULL,
   `v_secondname` varchar(30) NOT NULL,
   `v_email` varchar(30) NOT NULL,
-  `v_contact` bigint(20) NOT NULL,
+  `v_contact` int(20) NOT NULL,
   `v_address` varchar(60) NOT NULL,
   `v_password` varchar(30) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `volunteer`
---
-
-INSERT INTO `volunteer` (`v_id`, `v_firstname`, `v_secondname`, `v_email`, `v_contact`, `v_address`, `v_password`) VALUES
-(2, 'tisha', 'ttt', 'ttttt', 456, 'andheri', '444'),
-(3, 'kira', 'rr', 'rtty', 876, 'borivali', '4567'),
-(4, 'yu', 'yy', 'rrrrr', 9820360292, 'andheri', '12345'),
-(11, 'sadia', 'siddiqui', 'sas170298@gmail.com', 982, 'Borivali', 'eeeee');
 
 --
 -- Indexes for dumped tables
@@ -139,17 +129,20 @@ ALTER TABLE `volunteer`
 -- AUTO_INCREMENT for table `donor`
 --
 ALTER TABLE `donor`
-  MODIFY `d_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `d_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
 --
 -- AUTO_INCREMENT for table `food`
 --
 ALTER TABLE `food`
   MODIFY `food_id` int(11) NOT NULL AUTO_INCREMENT;
+
 --
 -- AUTO_INCREMENT for table `volunteer`
 --
 ALTER TABLE `volunteer`
-  MODIFY `v_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;COMMIT;
+  MODIFY `v_id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
--- php/way2sms-api.php
+++ php/way2sms-api.php
@@ -1,5 +1,13 @@
 <?php
 
+/**
+ * WAY2SMSClient
+ * @author Kingster
+ * @author SuyashBansal
+ * @category SMS
+ * Please use this code on your own risk. The author is no way responsible for the outcome arising out of this
+ * Good Luck!
+ **/
 class WAY2SMSClient
 {
 
@@ -14,27 +22,82 @@ class WAY2SMSClient
      * @param $password
      * @return bool|string
      */
-   
+    function login($username, $password)
+    {
+        $this->curl = curl_init();
+        $uid = urlencode($username);
+        $pwd = urlencode($password);
+
+        // Go where the server takes you :P
+        curl_setopt($this->curl, CURLOPT_URL, "http://way2sms.com");
+        curl_setopt($this->curl, CURLOPT_HEADER, true);
+        curl_setopt($this->curl, CURLOPT_FOLLOWLOCATION, false);
+        curl_setopt($this->curl, CURLOPT_RETURNTRANSFER, TRUE);
+        $a = curl_exec($this->curl);
+        if (preg_match('#Location: (.*)#', $a, $r))
+            $this->way2smsHost = trim($r[1]);
+
+        // Setup for login
+        curl_setopt($this->curl, CURLOPT_URL, $this->way2smsHost . "Login1.action");
+        curl_setopt($this->curl, CURLOPT_POST, 1);
+        curl_setopt($this->curl, CURLOPT_POSTFIELDS, "username=" . $uid . "&password=" . $pwd . "&button=Login");
+        curl_setopt($this->curl, CURLOPT_COOKIESESSION, 1);
+        curl_setopt($this->curl, CURLOPT_COOKIEFILE, "cookie_way2sms");
+        curl_setopt($this->curl, CURLOPT_FOLLOWLOCATION, 1);
+        curl_setopt($this->curl, CURLOPT_MAXREDIRS, 20);
+        curl_setopt($this->curl, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($this->curl, CURLOPT_USERAGENT, "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36");
+        curl_setopt($this->curl, CURLOPT_CONNECTTIMEOUT, $this->timeout);
+        curl_setopt($this->curl, CURLOPT_REFERER, $this->way2smsHost);
+        $text = curl_exec($this->curl);
+
+        // Check if any error occured
+        if (curl_errno($this->curl))
+            return "access error : " . curl_error($this->curl);
+
+        // Check for proper login
+        $pos = stripos(curl_getinfo($this->curl, CURLINFO_EFFECTIVE_URL), "main.action");
+        if ($pos === "FALSE" || $pos == 0 || $pos == "")
+            return "invalid login";
+
+        // Set the home page from where we can send message
+        $this->refurl = curl_getinfo($this->curl, CURLINFO_EFFECTIVE_URL);
+        /*$newurl = str_replace("ebrdg.action?id=", "main.action?section=s&Token=", $this->refurl);
+        curl_setopt($this->curl, CURLOPT_URL, $newurl);*/
+
+        // Extract the token from the URL
+        $tokenLocation = strpos($this->refurl, "Token");
+        $this->jstoken = substr($this->refurl, $tokenLocation + 6, 37);
+        //Go to the homepage
+        //$text = curl_exec($this->curl);
+
+        return true;
+    }
+
 
     /**
      * @param $phone
      * @param $msg
      * @return array
      */
-    function send($v_contact, $msg)
+    function send($phone, $msg)
     {
         $result = array();
 
         // Check the message
-        $msg="There is a nearby donor";
+        if (trim($msg) == "" || strlen($msg) == 0)
+            return "invalid message";
+
+        // Take only the first 140 characters of the message
+        $msg = substr($msg, 0, 140);
         // Store the numbers from the string to an array
-        $pharr = explode(",", $v_contact);
+        $pharr = explode(",", $phone);
 
         // Send SMS to each number
         foreach ($pharr as $p) {
             // Check the mobile number
             if (strlen($p) != 10 || !is_numeric($p) || strpos($p, ".") != false) {
-                $result[] = array('v_contact' => $p, 'msg' => $msg, 'result' => "invalid number");
+                $result[] = array('phone' => $p, 'msg' => $msg, 'result' => "invalid number");
                 continue;
             }
 
@@ -55,7 +118,16 @@ class WAY2SMSClient
     }
 
 
-   
+    /**
+     * logout of current session.
+     */
+    function logout()
+    {
+        curl_setopt($this->curl, CURLOPT_URL, $this->way2smsHost . "LogOut");
+        curl_setopt($this->curl, CURLOPT_REFERER, $this->refurl);
+        $text = curl_exec($this->curl);
+        curl_close($this->curl);
+    }
 
 }
 
@@ -64,10 +136,12 @@ class WAY2SMSClient
  * @example sendWay2SMS ( '9000012345' , 'password' , '987654321,9876501234' , 'Hello World')
  */
 
-function sendWay2SMS($v_contact, $msg)
+function sendWay2SMS($uid, $pwd, $phone, $msg)
 {
     $client = new WAY2SMSClient();
-    $result = $client->send($v_contact, $msg);
+    $client->login($uid, $pwd);
+    $result = $client->send($phone, $msg);
+    $client->logout();
     return $result;
 }
 
